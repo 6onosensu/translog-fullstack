a0f0ae332011468c98312d3c2d3ff405d03265d2
darja <darja.portnova@gmail.com>
2026-09-05 15:40:08 +0200

    set up database and user model

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService} from './app.service';
+import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+       inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow<string>('DB_HOST'),
+        port: Number(configService.getOrThrow<string>('DB_PORT')),
+        username: configService.getOrThrow<string>('DB_USERNAME'),
+        password: configService.getOrThrow<string>('DB_PASSWORD'),
+        database: configService.getOrThrow<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity } from "typeorm";
+import { BaseEntity } from "../../common/base.entity";
+import { UserRole } from "../enums/user-role.enum";
+
+@Entity('users')
+export class User extends BaseEntity { 
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  passwordHash!: string;
+
+  @Column({ 
+    type: 'enum',
+    enum: UserRole,
+  })
+  role!: UserRole;
+}
