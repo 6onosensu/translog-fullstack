a2f654af5cb660be8258094c1dc96b242d03a827
darja <darja.portnova@gmail.com>
2026-09-08 16:22:15 +0200

    clean up angular starter and set up login page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then(
+        (component) => component.LoginComponent,
+      ),
+  },
+];
---
+++ frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email = new FormControl('');
+}
