00df6efcb7bb6a2e5dfb3cb65562fd4e3ca83e5f
darja <darja.portnova@gmail.com>
2026-09-09 14:24:02 +0200

    style shipment status and date

--- frontend/src/app/pages/shipments/shipments.component.ts
+++ frontend/src/app/pages/shipments/shipments.component.ts
@@ -2,12 +2,14 @@ import { Component, inject } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { Shipment, ShipmentsService } from '../../services/shipments.service';
 import { MatTableModule } from '@angular/material/table';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-shipments',
   imports: [
     HeaderComponent,
     MatTableModule,
+    DatePipe,
   ],
   templateUrl: './shipments.component.html',
   styleUrl: './shipments.component.css'
